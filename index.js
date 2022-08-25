a20979acb86b2b1b8c9bf8395d5f6bfa33123e39
Svetkachalina <svetkachalina@gmail.com>
2022-08-25 18:42:22 +0300

    add a new category and product

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 'use strict';
+{
+    const item = 'smart проектор philips';
+    const count = 15;
+    const category = 'видео техника';
+    const price = 1200;
+    
+    console.log(item);
+    console.log(`общая сумма товара: ${count * price}`);
+    
+    /**
+      *   Создайте второй товар
+      *   Другой категории
+      */
+}
+
+{
+    const item = 'телефон SamsungXXX';
+    const count = 25;
+    const category = 'мобильные телефоны';
+    const price = 110;
+
+    console.log(item);
+    console.log(`общая сумма товара: ${count * price}`);
+}
+
